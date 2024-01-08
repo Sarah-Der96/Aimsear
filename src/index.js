5720a21d9a4efbb3fa6802eea13ed595e47e8d76
Sarah-Der96 <154680596+Sarah-Der96@users.noreply.github.com>
2024-01-08 18:38:10 +0000

    created forecast JS loop- injected HTML

--- src/index.js
+++ src/index.js
@@ -53,7 +53,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <img
+      class="weather-forecast-icon"
+      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+      alt=""
+      width="42px"
+    />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max"> 18° </span>
+      <span class="weather-forecast-temperature-min"> 12° </span>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Dublin");
+displayForecast();
